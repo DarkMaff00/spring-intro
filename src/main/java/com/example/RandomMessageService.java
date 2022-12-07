750c9b8fceac2e1237ed64b3e914d54b79eb43f8
Konrad Wojtowicz <konrad.wojtowicz@interia.pl>
2022-12-07 18:49:52 +0100

    After Hash

--- src/main/java/com/example/RandomMessageService.java
+++ src/main/java/com/example/RandomMessageService.java
@@ -1,7 +1,10 @@
 package com.example;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Random;
 
+@Component("randomMessageService")
 public class RandomMessageService implements MessageService {
 
     private final String[] messageArray = {"Message1", "Message2", "Message3", "Message4", "Message5", "Message6", "Message7", "Message8", "Message9", "Message10"};
